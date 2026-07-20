3c512a039994758867a5382431a4add1434f8e32
Bandaru Rohan <bandarurohan34@gmail.com>
2026-07-20 21:33:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.71%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         return n;
+        
         //gcd(a, b) = gcd(b, a mod b); repeat until remainder becomes 0 (ans will be min(a, b))
     }
 };
