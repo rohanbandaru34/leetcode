3cf6eb479906b16b0784e969701981bf32a5eaa4
Bandaru Rohan <bandarurohan34@gmail.com>
2026-08-02 23:12:40 +0530

    Time: 0 ms (100%), Space: 9 MB (36.47%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,22 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize - 1;
+    int ans = numsSize;
+    while (low <= high)
+    {
+    int mid = (low + high) / 2;
+       if (nums[mid] >= target)
+       {
+        ans = mid;
+        high = mid - 1;
+       }
+
+       else
+       {
+        low = mid + 1;
+       }
+    }
+
+
+    return ans;
+}
